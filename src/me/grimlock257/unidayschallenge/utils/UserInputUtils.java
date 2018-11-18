963435ab57b9c6cacf17acee7e6efeb741849a7f
Adam Watson <adamwatsonoa@gmail.com>
2018-11-18 18:41:05 +0000

    Create UserInputUtils.java
    - Method to get validated integer input from the user between specified min and max

---
+++ src/me/grimlock257/unidayschallenge/utils/UserInputUtils.java
@@ -0,0 +1,32 @@
+package me.grimlock257.unidayschallenge.utils;
+
+import java.util.Scanner;
+
+public class UserInputUtils {
+    public static int getIntegerInput(Scanner scanner, int min, int max) {
+        int userOption;
+
+        // Loop forever until valid selection is made
+        while (true) {
+            System.out.print("> ");
+
+            // Check if there is an int in the scanner buffer, else say they need to enter an integer
+            if (scanner.hasNextInt()) {
+                userOption = scanner.nextInt();
+            } else {
+                System.out.println("You must enter an integer, please select a valid option (" + min + " to " + max + ")");
+                scanner.next();
+                continue;
+            }
+
+            // We know a integer has been input, is it within the specified bounds?
+            if (userOption >= min && userOption <= max) {
+                break;
+            }
+
+            System.out.println("Invalid selection, please select a valid option (" + min + " to " + max + ")");
+        }
+
+        return userOption;
+    }
+}
